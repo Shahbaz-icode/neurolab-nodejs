07d3e20377555dd333352d32f60d4a80c3ce9fee
Shahbaz <ansari.shahbaz13@hotmail.com>
2024-02-06 00:05:50 +0530

    modified:   NewApp/client/package-lock.json 	modified:   NewApp/client/package.json 	modified:   NewApp/client/src/App.js 	new file:   NewApp/client/src/components/Form/Form.js 	new file:   NewApp/client/src/components/Form/styles.js 	new file:   NewApp/client/src/components/Posts/Post/Post.js 	new file:   NewApp/client/src/components/Posts/Post/styles.js 	new file:   NewApp/client/src/components/Posts/Posts.js 	new file:   NewApp/client/src/components/Posts/styles.js 	new file:   NewApp/client/src/images/memories.png 	new file:   NewApp/server/controllers/posts.js 	new file:   NewApp/server/models/postMessage.js 	modified:   NewApp/server/routes/posts.js

--- NewApp/client/src/App.js
+++ NewApp/client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Container, Nav, Typography } from 'react-bootstrap'
 
+import memories from './images/memories.png'
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <Nav poaition="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"/>
+            </Nav>
+        </Container>
     );
 }
 
